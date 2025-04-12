53d12a30100ad5ff86fee868805ede0061f22a78
João Giatti <joaogiatti2006@gmail.com>
2025-04-12 19:50:04 -0300

    part 2: Add multiple texts

--- app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
@@ -17,19 +17,24 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent{
-            MessageCard("Android")
+            MessageCard(Message("Android", "Jetpack Compose"))
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
-fun MessageCard(name: String){
-    Text(text = "hello $name")
+fun MessageCard(msg: Message){
+    Text(text = msg.author)
+    Text(text = msg.body)
 }
 
 // Preview é uma função que permite visualizar o componente em tempo real, ela deve ser delcarada em funções sem parâmetros
 @Preview
 @Composable
-fun PreviewMessageCard(){
-    MessageCard("Android")
+fun PreviewMessageCard() {
+    MessageCard(
+        msg = Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!")
+    )
 }
